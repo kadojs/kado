e2bc70ac4831e96eef6bca42aac6cd3f7e85ac04
Bryan Tong <contact@nullivex.com>
2015-08-26 16:16:42 -0600

    initial commit

---
+++ core/helpers/list.js
@@ -0,0 +1,51 @@
+'use strict';
+var P = require('bluebird')
+var validator = require('validator')
+
+
+/**
+ * Remove members from a list
+ * @param {sequelize.Model} Model
+ * @param {array} items
+ * @return {P}
+ */
+exports.remove = function(Model,items){
+  return P.try(function(){
+    if(!(items instanceof Array))
+      throw new Error('Invalid data passed for record removal')
+    var promises = []
+    var i = items.length - 1
+    for(; i >= 0; i--){
+      if(validator.isNumeric(items[i])){
+        promises.push(Model.destroy({where: {id: items[i]}}))
+      }
+    }
+    return P.all(promises)
+  })
+}
+
+
+/**
+ * Pagination helper
+ * @param {number} start
+ * @param {number} count
+ * @param {number} limit
+ * @return {{start: *, end: *, previous: number, next: *}}
+ */
+exports.pagination = function(start,count,limit){
+  if(start > count) start = count - limit
+  var page = {
+    start: start,
+    end: start + limit,
+    previous: start - limit,
+    next: start + limit,
+    first: 0,
+    last: (Math.ceil(count / limit) * limit) - limit,
+    count: count,
+    limit: limit
+  }
+  if(page.previous < 0) page.previous = 0
+  if(page.next > count) page.next = start
+  if(page.end > count) page.end = count
+  return page
+}
