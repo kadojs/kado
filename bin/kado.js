5892f57c65260739d5640b69cb5ce84ffb2ecd0c
Bryan Tong <contact@nullivex.com>
2018-02-19 20:35:15 -0700

    fix bootstrap command line to handle module enabling

--- bin/kado.js
+++ bin/kado.js
@@ -10,43 +10,57 @@ program.command('bootstrap')
   .option('--enable-blog')
   .option('--enable-setting')
   .option('--enable-user')
-  .action(function(){
+  .action(function(cmd){
     var folder = process.cwd()
     var appFile = path.resolve(folder + '/app.js')
     if(fs.existsSync(appFile)){
       console.log('ERROR app file already exits')
       process.exit(1)
     }
+    var moduleConfig = ''
+    var enableModule = function(name,flag){
+      if(flag){
+        var isFirst = false
+        if(!moduleConfig){
+          moduleConfig = ',\n  module: {\n'
+          isFirst = true
+        }
+        moduleConfig = moduleConfig +
+          (isFirst ? '' : ',\n') + '    ' + name + ': { enabled: true }'
+      }
+    }
+    enableModule('blog',cmd.enableBlog)
+    enableModule('setting',cmd.enableSetting)
+    enableModule('user',cmd.enableUser)
+    if(moduleConfig) moduleConfig = moduleConfig + '\n  }\n'
     var appData = '\'use strict\';\n' +
       'var K = require(\'kado\');\n' +
       'K.configure({\n' +
       '  root: __dirname,\n' +
       '  interface: {\n' +
-      '    admin: {\n' +
-      '      enabled: true\n' +
-      '    }\n' +
-      '  }\n' +
+      '    admin: { enabled: true }\n' +
+      '  }' + moduleConfig +
       '})\n' +
       '\n' +
-      'if(require.main === module){' +
-      '  infant.child(' +
-      '    \'myapp\',' +
-      '    function(done){' +
-      '      K.start(function(err){' +
-      '        if(err) return done(err)' +
-      '        K.log.info(\'Kado started!\')' +
-      '        done()' +
-      '      })' +
-      '    },' +
-      '    function(done){' +
-      '      K.stop(function(err){' +
-      '        if(err) return done(err)' +
-      '        K.log.info(\'Kado stopped!\')' +
-      '        done()' +
-      '      })' +
-      '    }' +
-      '  )' +
-      '}'
+      'if(require.main === module){\n' +
+      '  K.infant.child(\n' +
+      '    \'myapp\',\n' +
+      '    function(done){\n' +
+      '      K.start(function(err){\n' +
+      '        if(err) return done(err)\n' +
+      '        K.log.info(\'Kado started!\')\n' +
+      '        done()\n' +
+      '      })\n' +
+      '    },\n' +
+      '    function(done){\n' +
+      '      K.stop(function(err){\n' +
+      '        if(err) return done(err)\n' +
+      '        K.log.info(\'Kado stopped!\')\n' +
+      '        done()\n' +
+      '      })\n' +
+      '    }\n' +
+      '  )\n' +
+      '}\n'
     fs.writeFileSync(appFile,appData)
     console.log('Application is ready!')
     process.exit()
