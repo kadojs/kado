4da51463ec024f6e0cebedcce32eadfa00667653
JustFowl <jfowl.2018@gmail.com>
2019-11-24 20:44:34 -0700

    nav test

---
+++ test/Nav.test.js
@@ -0,0 +1,38 @@
+'use strict';
+/**
+ * Kado - Web Application System
+ * Copyright © 2015-2019 Bryan Tong, NULLIVEX LLC. All rights reserved.
+ * Kado <support@kado.org>
+ *
+ * This file is part of Kado and bound to the MIT license distributed within.
+ */
+
+const { expect } = require('chai')
+const Nav = require('../lib/Nav')
+let nav = new Nav()
+
+describe('Nav',()=> {
+  it('should construct',() => {
+    let testAsset = new Nav()
+    expect(testAsset).to.be.an('object')
+  })
+  it('should be empty',() => {
+    expect(Object.keys(nav.all()).length).to.equal(0)
+    expect(nav.allGroups().length).to.equal(0)
+  })
+  it('should add a nav group',() => {
+    expect(nav.addGroup('/test','Test', 'fa fa-plus').uri).to.equal('/test')
+
+  })
+  it('should get nav entry by name', () => {
+    expect(Object.keys(nav.all()).length).to.equal(1)
+  })
+  it('should add Nav Item', () => {
+    expect(nav.addItem('Test','/test', 'Test').uri).to.equal('/test')
+  })
+  it('should return built nav entries' , () => {
+    expect(Object.keys(nav.all()).length).to.equal(1)
+  console.log(nav.all())
+  })
+})
+
