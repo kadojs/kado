7ebfef54952ada19a103dfbfbaa0855dfdae588f
Nullivex <bryan@nullivex.com>
2019-11-24 19:28:30 -0700

    logger testing, message testing

---
+++ test/Logger.test.js
@@ -0,0 +1,34 @@
+'use strict';
+/**
+ * Kado - Web Application System
+ * Copyright © 2015-2019 Bryan Tong, NULLIVEX LLC. All rights reserved.
+ * Kado <support@kado.org>
+ *
+ * This file is part of Kado and bound to the MIT license distributed within.
+ */
+
+const { expect } = require('chai')
+const Logger = require('../lib/Logger')
+let logger = new Logger()
+
+describe('Event',()=> {
+  it('should construct',() => {
+    let testLogger = new Logger()
+    expect(testLogger).to.be.an('object')
+  })
+  it('should have no logger',() => {
+    expect(logger.getLogger()).to.equal(null)
+  })
+  it('should setup a logger',() => {
+    expect(logger.setup('kado','mm:hh:ss MM-DD-YYY')).to.be.an('object')
+  })
+  it('should have a logger',() => {
+    expect(logger.getLogger()).to.be.an('object')
+  })
+  it('should reset',() => {
+    expect(logger.reset()).to.equal(true)
+  })
+  it('should now not have a logger',() => {
+    expect(logger.getLogger()).to.equal(null)
+  })
+})
---
+++ test/Message.test.js
@@ -0,0 +1,49 @@
+'use strict';
+/**
+ * Kado - Web Application System
+ * Copyright © 2015-2019 Bryan Tong, NULLIVEX LLC. All rights reserved.
+ * Kado <support@kado.org>
+ *
+ * This file is part of Kado and bound to the MIT license distributed within.
+ */
+
+const { expect } = require('chai')
+const Message = require('../lib/Message')
+let message = new Message()
+
+describe('Message',()=> {
+  it('should construct',() => {
+    let testMessage = new Message()
+    expect(testMessage).to.be.an('object')
+  })
+  it('should have no handlers',()=>{
+    expect(Object.keys(message.allHandlers()).length).to.equal(0)
+  })
+  it('should add a handler',()=>{
+    expect(message.addHandler('test',(options)=>{
+      return options
+    }))
+  })
+  it('should have a handler',()=>{
+    expect(message.getHandler('test')).to.be.an('object')
+  })
+  it('should remove a handler',()=>{
+    expect(message.removeHandler('test')).to.equal('test')
+  })
+  it('should have no handlers',()=>{
+    expect(Object.keys(message.allHandlers()).length).to.equal(0)
+  })
+  it('should accept a new handler',()=>{
+    expect(message.addHandler('test',(options)=>{
+      return options
+    }))
+  })
+  it('should send a message and see it in the handler',()=>{
+    return message.send('foo@foo.com','something to do')
+      .then((result)=>{
+        result = result[0]
+        expect(result.to).to.equal('foo@foo.com')
+        expect(result.text).to.equal('something to do')
+      })
+  })
+})
