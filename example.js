280b0df51abd251281923e2b293ec34c771a3758
Nullivex <bryan@nullivex.com>
2019-11-26 21:39:06 -0700

    update example and disable sequelize by default

--- example.js
+++ example.js
@@ -1,16 +1,19 @@
 'use strict';
 const Kado = require('./lib/Kado')
-const app = new Kado(require('./config.js'))
+const app = new Kado()
 const port = 3000
 
-app.get('/',
-  (req,res,next)=>{console.log('some middleware'); next()},
-  (req,res)=>{res.send('Hello World')}
-)
+app.get('/',(req,res)=>{
+  res.send('Hello World')
+})
 
-app.listen(port)
+app.start()
   .then((result)=>{
-    if(result !== 'CLI Mode') app.log.info('Kado Started on port ' + port)
+    if(result !== 'CLI Mode')
+    app.listen(port,(err)=>{
+      if(err) throw err
+      app.log.info('Kado Started on port ' + port)
+    })
   })
   .catch((e)=>{
     app.log.error('Startup failed: ' + e.message)
