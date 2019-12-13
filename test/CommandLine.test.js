141d337751d69624a5704183afbb1ae11e4382b7
Nullivex <bryan@nullivex.com>
2019-12-12 22:11:36 -0700

    modify how modules are routed with the command line system

--- test/CommandLine.test.js
+++ test/CommandLine.test.js
@@ -25,6 +25,7 @@ const testArgs = [
   process.argv[0],
   process.argv[1],
   'test',
+  'test',
   '-t','test'
 ]
 
@@ -37,16 +38,16 @@ describe('CommandLine',()=> {
     expect(Object.keys(cli.all()).length).to.equal(0)
   })
   it('should add a command',() => {
-    expect(cli.command('test',testCommand)).to.equal('test')
+    expect(cli.command('test','test',testCommand)).to.equal('test')
   })
   it('should have the command',() => {
-    expect(cli.getCommand('test')).to.be.an('object')
+    expect(cli.getCommand('test','test')).to.be.an('object')
   })
   it('should remove the command',() => {
-    expect(cli.removeCommand('test')).to.equal('test')
+    expect(cli.removeCommand('test','test')).to.equal('test')
   })
   it('should should add a new command',() => {
-    expect(cli.command('test',testCommand)).to.equal('test')
+    expect(cli.command('test','test',testCommand)).to.equal('test')
   })
   it('should show in all commands',() => {
     expect(cli.all().test).to.be.an('object')
