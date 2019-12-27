2e631f1ce06614758b7c6c97eb9fba87e5b202df
Nullivex <bryan@nullivex.com>
2019-12-27 12:51:18 -0700

    add first portion of abstraction for HyperText engines

---
+++ test/HyperText.test.js
@@ -0,0 +1,110 @@
+'use strict';
+/**
+ * Kado - Web Application System
+ * Copyright © 2015-2019 Bryan Tong, NULLIVEX LLC. All rights reserved.
+ * Kado <support@kado.org>
+ *
+ * This file is part of Kado and bound to the MIT license distributed within.
+ */
+
+const { expect } = require('chai')
+const Kado = require('../lib/Kado')
+const app = new Kado()
+const HyperText = require('../lib/HyperText')
+const hyperText = new HyperText(app)
+class OurEngine extends hyperText.HyperTextEngine {
+  constructor(){
+    super()
+    this.express = require('express')()
+    this.http = require('http').createServer(this.express)
+  }
+  start(port,host){
+    super.start()
+    super.checkPort(port)
+    super.checkHost(host)
+    const that = this
+    return new Promise((resolve,reject)=> {
+      that.http.listen(port,host,(err) => {
+        if(err) return reject(err)
+        resolve(that.http)
+      })
+    })
+  }
+  stop(){
+    super.stop()
+    const that = this
+    return new Promise((resolve,reject)=> {
+      that.http.close((err) => {
+        if(err) return reject(err)
+        resolve(true)
+      })
+    })
+  }
+}
+function checkServer(port,host,uri){
+  const http = require('http')
+  return new Promise((resolve,reject)=>{
+    const params = {
+      port: port,
+      host: host,
+      method: 'GET',
+      path: uri
+    }
+    const req = http.request(params)
+    req.end()
+    req.on('response',(res)=> {
+      let data = null
+      res.setEncoding(('utf8'))
+      res.on('data',(chunk)=>{ data += chunk })
+      res.on('end',()=>{
+        resolve(expect(data).to.match(/Cannot GET \//))
+      })
+      res.on('error',reject)
+    })
+    req.on('error',reject)
+  })
+}
+
+
+describe('HyperText',()=> {
+  it('should construct',() => {
+    let testHyperText = new HyperText(app)
+    expect(testHyperText).to.be.an('object')
+  })
+  it('should have no handlers',() => {
+    expect(Object.keys(hyperText.allHandlers()).length).to.equal(0)
+  })
+  it('should add a handler',() => {
+    expect(hyperText.addHandler('express',new OurEngine())).to.equal('express')
+  })
+  it('should have a handler',() => {
+    expect(hyperText.getHandler('express')).to.be.an('object')
+  })
+  it('should have 1 total handlers',() => {
+    expect(Object.keys(hyperText.allHandlers()).length).to.equal(1)
+  })
+  it('should remove a handler',() => {
+    expect(hyperText.removeHandler('express')).to.equal('express')
+  })
+  it('should have no handlers',() => {
+    expect(Object.keys(hyperText.allHandlers()).length).to.equal(0)
+  })
+  it('should accept a new handler',() => {
+    expect(hyperText.addHandler('ex',new OurEngine())).to.equal('ex')
+  })
+  it('should have the new handler',() => {
+    expect(hyperText.getHandler('ex')).to.be.an('object')
+  })
+  it('should activate the new handler',() => {
+    expect(hyperText.activateHandler('ex')).to.equal('ex')
+  })
+  it('should start the engine',async () => {
+    expect(await hyperText.start(3000,'localhost')).to.be.an('object')
+  })
+  it('should be listening with the engine',async () => {
+    expect(await checkServer(3000,'localhost','/'))
+  })
+  it('should stop the engine',async () => {
+    expect(await hyperText.stop()).to.equal(true)
+  })
+})
