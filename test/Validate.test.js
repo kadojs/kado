87fecbd06075d00a89a90ccd03eec7e73427100e
JustFowl <jfowl.2018@gmail.com>
2019-12-29 18:33:05 -0700

    validate.test.js Asset complete

--- test/Validate.test.js
+++ test/Validate.test.js
@@ -22,45 +22,144 @@
 describe('Validate',()=>{
   const { expect } = require('chai')
   const Val = require('../lib/Validate')
+  const Assert = Val.Assert
+  const AssertionError = Val.AssertionError
   describe('assert',()=>{
-    it('should assert an array')
-    it('should assert an object')
-    it('should assert an date')
-    it('should assert an error')
-    it('should assert an function')
-    it('should assert an string')
-    it('should assert an number')
-    it('should assert an null')
-    it('should assert an undefined')
-    it('should assert an boolean')
+    it('should assert an array', () => {
+      Assert.eqDeep([],[])
+      Assert.eqDeep.catch([],[1],'Array() does not deep equal Array(1)')
+      Assert.eq.catch([],{},
+        'Array() does not reference equal Object([object Object])')
+    })
+    it('should assert a boolean',() => {
+      Assert.eq(true,true)
+      Assert.eq.catch(true,false,'boolean(true) does not equal boolean(false)')
+    })
+    it('should assert a date',() => {
+      Assert.assert(new Date(), new Date())
+    })
+    it('should fail two new dates over time',async ()=>{
+      function getDate(){ return new Promise((resolve)=>{
+        setTimeout(()=>{ resolve(new Date()) },2)
+      }) }
+      Assert.assert.catch(new Date(), await getDate(),AssertionError)
+    })
+    it('should assert an error', () => {
+      Val.assert(Error, Error)
+    })
+    it('should assert a function',() => {
+      Val.assert(() => {}, () => {})
+    })
+    it('should assert a null',() => {
+      Val.assert(null,null)
+      Val.catch(Val.assert.bind(null,null,undefined),
+        'Assertion failed null does not equal undefined')
+    })
+    it('should assert a number',() => {
+      Val.assert(1,1)
+    })
+    it('should assert an object',() => {
+      Val.assert({},{})
+    })
+    it('should assert a string',() => {
+      Val.assert('','')
+      Val.assert('foo','foo')
+    })
+    it('should assert an undefined',() => {
+      Val.assert(undefined,undefined)
+      Val.catch(Val.assert.bind(null,undefined,null),
+        'Assertion failed undefined does not equal null')
+    })
   })
   describe('eq',()=>{
-    it('should eq string')
-    it('should eq null')
-    it('should eq boolean')
-    it('should eq array')
-    it('should eq object')
+    it('should eq array',() => {
+      const arr1 = []
+      Assert.eq(arr1,arr1)
+      Assert.eq.catch({},{},
+        'Object([object Object]) does not ' +
+        'reference equal Object([object Object])')
+      Assert.eq.catch([],[],'Array() does not reference equal Array()')
+      Assert.eq.catch([],{},
+        'Array() does not reference equal Object([object Object])')
+    })
+    it('should eq boolean',() => {
+      Val.eq(true,true)
+      Val.catch(Val.eq.bind(null,true,false),
+        'Assertion failed true does not equal false')
+    })
+    it('should eq null',() => {
+      Val.eq(null,null)
+      Val.catch(Val.eq.bind(null,null,undefined),
+        'Assertion failed null does not equal undefined')
+    })
+    it('should eq object',() => {
+      Val.eq({},{})
+      Val.catch(Val.eq.bind(null,{},[]),
+        'Assertion failed {} does not equal []')
+    })
+    it('should eq string',() =>{
+    Val.eq('','')
+    Val.eq('foo','foo')
+    })
   })
   describe('eqDeep',()=>{
-    it('should eq an object')
-    it('should eq an array')
+    it('should eq an object',() => {
+      Assert.eq(Val.eqDeep({},{}))
+      Assert.eq(Val.eqDeep({},[]),false)
+    })
+    it('should eq an array',()=>{
+      Assert.eq(Val.eqDeep([],[]))
+      Assert.eq(Val.eqDeep([],{}),false)
+    })
   })
   describe('getType',()=>{
-    it('should get a string')
-    it('should get a number')
-    it('should get a array')
-    it('should get a object')
-    it('should get a null')
-    it('should get a undefined')
-    it('should get a boolean')
+    it('should get a string',()=>{
+      Assert.eq(Val.getType(''),'string')
+    })
+    it('should get a number',()=>{
+      Assert.eq(Val.getType(1),'number')
+    })
+    it('should get a array',()=>{
+      Assert.eq(Val.getType([]),'Array')
+    })
+    it('should get a object',()=>{
+      Assert.eq(Val.getType({}),'Object')
+    })
+    it('should get a null',()=>{
+      Assert.eq(Val.getType(null),'null')
+    })
+    it('should get a undefined',()=>{
+      Assert.eq(Val.getType(),'undefined')
+      Assert.eq.catch(Val.getType(''),'undefined',
+        'string(string) does not equal string(undefined)')
+    })
+    it('should get a boolean',()=>{
+      Assert.eq(Val.getType(true),'boolean')
+      Assert.eq.catch(Val.getType('false'),'boolean',
+        'string(string) does not equal string(boolean)')
+      Assert.eq.catch(Val.getType('+00000000'),'boolean',
+        'string(string) does not equal string(boolean)')
+    })
   })
   describe('isType',()=>{
-    it('should identify a string')
-    it('should identify a number')
-    it('should identify a array')
-    it('should identify a object')
-    it('should identify a null')
-    it('should identify a undefined')
+    it('should identify a string',()=>{
+      Assert.eq(Val.isType('string',''))
+    })
+    it('should identify a number',()=>{
+      Assert.eq(Val.isType('number',1))
+    })
+    it('should identify a array',()=>{
+      Assert.eq(Val.isType('Array',[]))
+    })
+    it('should identify a object',()=>{
+      Assert.eq(Val.isType('Object',{}))
+    })
+    it('should identify a null',()=>{
+      Assert.eq(Val.isType('null',null))
+    })
+    it('should identify an undefined',()=>{
+      Assert.eq(Val.isType('undefined',undefined))
+    })
   })
   describe('isAbove',()=>{
     it('should fail on invalid input')
@@ -86,16 +185,20 @@ describe('Validate',()=>{
     it('should be false on true neq true')
   })
   describe('getInstance',()=>{
-    it('should return an instance')
+    it('should return an instance',()=>{
+      Assert.isType('Validate',Val.getInstance())
+    })
   })
   describe('with instance',()=>{
     it('should check above',()=>{
-      Val.assert(new Val(5).above(4),true)
+      Assert.eq(new Val(5).above(4))
     })
     it('should check below',()=>{
-      Val.assert(new Val(5).below(6),true)
+      Assert.eq(new Val(5).below(6))
     })
-    it('should check equal')
+    it('should check equal',()=>{
+      Assert.eq(new Val(1).equal(1))
+    })
     it('should check is type')
     it('should check min')
     it('should check max')
@@ -154,7 +257,8 @@ describe('Validate',()=>{
     Val.assert(Val.eqDeep('',''),true)
     Val.assert(Val.eqDeep('foo','foo'),true)
     // ***Have Bryan check line 78 syntax***
-    Val.assert.bind(Val.eqDeep(null,'foo','bar'),'Assertion failed foo does not equal bar')
+    Val.assert.bind(Val.eqDeep(null,'foo','bar'),
+      'Assertion failed foo does not equal bar')
   })
   it('should check for Symbol')
   it('should check for SyntaxError')
@@ -163,6 +267,7 @@ describe('Validate',()=>{
   it('should check for Undefined', () => {
     Val.assert(Val.eqDeep(undefined,undefined),true)
     // ***Have Bryan check line 87 syntax***
-    Val.assert.bind(Val.eqDeep(null,undefined,null),'Assertion failed undefined does not equal null')
+    Val.assert.bind(Val.eqDeep(null,undefined,null),
+      'Assertion failed undefined does not equal null')
   })
 })
