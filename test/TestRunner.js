df2fc2db59a60aa4c3d6e329646037856ff3e51f
Nullivex <bryan@nullivex.com>
2020-01-01 17:36:06 -0700

    finalize TestRunner and add testing for it on the build chain, this puts the rest of the testing into limbo because all the tests now need converted to the new method before we can restructure the testing command

---
+++ test/TestRunner.js
@@ -0,0 +1,34 @@
+'use strict';
+const runner = require('../lib/TestRunner').getInstance('Kado')
+const { expect } = require('../lib/Validate')
+const test = runner.suite('suite1')
+runner.before(()=>{ console.log('stuff before running tests') })
+test.after(()=>{ console.log('some stuff after the suite ') })
+runner.after(()=>{ console.log('some stuff after the tests') })
+test.it('should do stuff',()=>{ expect.eq(true) })
+test.it('should do stuff 1',()=>{ expect.eq('true','true') })
+test.it('should do stuff 2',()=>{ expect.eq(1,1) })
+test.it('should do stuff 3',()=>{
+  return new Promise((resolve)=>{ setTimeout(()=>{
+    resolve(expect.eq(null,null))
+  },15) })
+})
+test.it('should do stuff 4',()=>{ expect.eq(false) })
+test.it('should do stuff 5',()=>{ throw new Error('foo') })
+//suites should recurse
+const test2 = test.suite('something nested')
+test2.it('should do more stuff',()=>{ expect.eq() })
+//and again
+const test3 = test2.suite('something really deep')
+test3.it('that is what she said',()=>{ expect.eq() })
+runner.test('something out of band',()=>{ expect.eq(false,false) })
+runner.test('something out of band 2',()=>{ expect.eq(false,true) })
+runner.execute()
+  .then(code => {
+    expect.eq(code,3)
+    console.log('TestRunner test passing!')
+  })
+  .catch((e)=>{
+    console.log(`TestRunner testing failed: ${e.message}`)
+    process.exit(1)
+  })
