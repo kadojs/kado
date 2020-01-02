7850c8d247fb8c439b921bfa6fbad262485303c3
Nullivex <bryan@nullivex.com>
2020-01-01 21:27:40 -0700

    rename test usage in TestRunner.test.js to avoid mocha conflicts

--- test/TestRunner.test.js
+++ test/TestRunner.test.js
@@ -1,28 +1,26 @@
 'use strict';
 const runner = require('../lib/TestRunner').getInstance('Kado')
 const { expect } = require('../lib/Validate')
-//jshint -W079
-const test = runner.suite('suite1')
-//jshint +W079
+const suite1 = runner.suite('suite1')
 const getOne = function(){ return new Promise((resolve)=>{
   setTimeout(()=>{ resolve(1) },5)
 }) }
 runner.before(()=>{ console.log('stuff before running tests') })
-test.after(()=>{ console.log('some stuff after the suite ') })
+suite1.after(()=>{ console.log('some stuff after the suite ') })
 runner.after(()=>{ console.log('some stuff after the tests') })
-test.it('should do stuff',()=>{ expect.eq(true) })
-test.it('should do stuff 1',()=>{ expect.eq('true','true') })
-test.it('should do stuff 2',async ()=>{ expect.eq(await getOne(),1) })
-test.it('should do stuff 3',function(){
+suite1.it('should do stuff',()=>{ expect.eq(true) })
+suite1.it('should do stuff 1',()=>{ expect.eq('true','true') })
+suite1.it('should do stuff 2',async ()=>{ expect.eq(await getOne(),1) })
+suite1.it('should do stuff 3',function(){
   this.timeout(5)
   return new Promise((resolve)=>{ setTimeout(()=>{
     resolve(expect.eq(null,null))
   },15) })
 })
-test.it('should do stuff 4',()=>{ expect.eq(false) })
-test.it('should do stuff 5',()=>{ throw new Error('foo') })
+suite1.it('should do stuff 4',()=>{ expect.eq(false) })
+suite1.it('should do stuff 5',()=>{ throw new Error('foo') })
 //suites should recurse
-const test2 = test.suite('something nested')
+const test2 = suite1.suite('something nested')
 test2.it('should do more stuff',()=>{ expect.eq() })
 //and again
 const test3 = test2.suite('something really deep')
