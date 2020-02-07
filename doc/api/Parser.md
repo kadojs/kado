a810f9f4f425e141513fdbd47dbbbb33f556b2df
Nullivex <bryan@nullivex.com>
2020-02-06 19:08:05 -0700

    Abstract lineToObject parser and add query string testing.

--- doc/api/Parser.md
+++ doc/api/Parser.md
@@ -9,7 +9,21 @@ save time and ensure a proper and secure parse.
 
 ## Class: Parser
 
+### static Parser.lineToObject(input, options)
+* `input` {string} Line of characters to parse into an object.
+* `options` {object} Settings to control the parser.
+* Return {object} containing parsed properties and values.
+
+Available Options
+* `delimiter` {string} used to denote end of values default `;`
+* `separator` {string} used to denote end of names default `=`
+* `whitespace` {boolean} used to allow whitespace after delimiter default `true`
+
 ### static Parser.cookie(input)
 * `input` {string} Usually sent via HTTP header containing available cookies.
 * Return {object} Where cookie names are properties and values are stored to
 those properties.
+
+### static Parser.queryString(input)
+* `input` {string} The portion after the `?` on a URL
+Return {object} Where names are properties and values are assigned.
--- test/Parser.test.js
+++ test/Parser.test.js
@@ -24,6 +24,8 @@ const Parser = require('../lib/Parser')
 const cookieString = '_ga=GA1.2.637651231.1575923282;' +
   ' mm2_cookieA=06bad8dd-ecda-4e00-abbe-1e641adde6f0; _ym_d=1586356006;' +
   ' _ym_uid=1576356006128031579; __qca=P0-1332394709-1576356006094'
+const queryString = 'view=choose&term=24&type=InternetPhone&configPage=true' +
+  '&pageVisited=preBundlePage'
 runner.suite('Parser', (it) => {
   it('should parse cookies', () => {
     const rv = Parser.cookie(cookieString)
@@ -33,6 +35,15 @@ runner.suite('Parser', (it) => {
     Assert.eq(rv._ym_d, '1586356006')
     Assert.eq(rv._ym_uid, '1576356006128031579')
     Assert.eq(rv.__qca, 'P0-1332394709-1576356006094')
+  })
+  it('should parse query strings', () => {
+    const rv = Parser.queryString(queryString)
+    Assert.isType('Object', rv)
+    Assert.eq(rv.view, 'choose')
+    Assert.eq(rv.term, '24')
+    Assert.eq(rv.type, 'InternetPhone')
+    Assert.eq(rv.configPage, 'true')
+    Assert.eq(rv.pageVisited, 'preBundlePage')
   })
 })
 if (require.main === module) runner.execute().then(code => process.exit(code))
