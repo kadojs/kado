3ab232e2a9e55470388a9e98d1bdcc96f9438a40
Drew Gordon <Drew@nullivex.com>
2020-03-04 17:19:50 -0700

    fixed Mime doc error and Added text. Removed dependencies line from package.test.js. Removed stray 5 from parser.test.js

--- test/Parser.test.js
+++ test/Parser.test.js
@@ -13,7 +13,7 @@
  * Kado is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.5
+ * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
  * along with Kado.  If not, see <https://www.gnu.org/licenses/>.
