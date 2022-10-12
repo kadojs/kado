cdcf7d9e69f19389f15e6130d32afa0d8ae6da57
Nullivex <bryan@nullivex.com>
2022-10-12 16:26:22 -0600

    Correct whitespace issue.

--- geoip-update.js
+++ geoip-update.js
@@ -1,9 +1,9 @@
 const argv = process.argv.filter((v, i, a) => {
-    return !(
-      v.indexOf(process.argv0) === v.length - process.argv0.length ||
-      v.indexOf(__filename.replace(/\.js$/, '')) === 0
-    )
-  })
+  return !(
+    v.indexOf(process.argv0) === v.length - process.argv0.length ||
+    v.indexOf(__filename.replace(/\.js$/, '')) === 0
+  )
+})
 ;(async () => await require('./lib/GeoIPUpdate')
   .getInstance()
   .updateData(argv[0] || 'free'))()
